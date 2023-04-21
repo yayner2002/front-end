6e5aec70531af20fc37558e2fe1c5b9c2cc97d0c
yayner2002 <yayner2002@gmail.com>
2023-04-22 00:22:59 +0300

    Install and configure react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
-function App() {
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello react</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Users />} />
+        <Route path="/places/new" element={<NewPlace />}/>
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
