fdb7189006a1a4a5a67647c5aaf3d4ac8d62e0fc
yayner2002 <yayner2002@gmail.com>
2023-04-23 23:12:57 +0300

    Add Backdrop component

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
 import "./MainNavigation.css";
+import Backdrop from "../UIElements/Backdrop";
 import SideDrawer from "./SideDrawer";
 
 const MainNavigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const openDrawer = () => {
+    setDrawerIsOpen(true)
+  }
+  const closeDrawer = () => {
+    setDrawerIsOpen(false)
+  }
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawer}
+        >
           <span></span>
           <span></span>
           <span></span>
